0af4777ca77562e20fb7ab951f51e477175819eb
felixf <felix@thealbatross.co.uk>
2019-08-29 12:23:06 +0100

    make work

--- javascript.js
+++ javascript.js
@@ -11,11 +11,16 @@ function minutesUntilMidnight() {
     return ( midnight.getTime() - new Date().getTime() ) / 1000 / 60;
 }
 
-
+function newQuote1() {
+  newQuote()
+}
+function newQuote2() {
+  newQuote()
+}
 
 function newQuote() {
   rand1 = Math.floor(Math.random()*(4));
 
-  document.getElementById('scanStatus').innerHTML = "Scan Started. Viruses found in last searched folder:"+=rand1;
-  document.getElementById('timeStatus').innerHTML = "Estimated finish time:" minutesUntilMidnight();
+  document.getElementById('scanStatus').innerHTML = "Scan Started. \n Viruses found in last searched folder:"+rand1;
+  document.getElementById('timeStatus').innerHTML = "\nEstimated finish time: " + Math.floor(minutesUntilMidnight()) +" minutes. Please do not use change any files in this time";
 }
